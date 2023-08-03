f58ec6db9d748dd544e9b9f76db6199538753ce1
Gurpreet Chugh <chugh.gurpreet@gmail.com>
2023-08-02 20:35:24 -0400

    updated flask

--- Flask App/static/js/teams.js
+++ Flask App/static/js/teams.js
@@ -131,14 +131,17 @@ function predTable(team) {
 
 // creating function to plot Bullet chart using plotly
 
-// function bulletChart(team) {
+function bulletChart(team) {
 
-//   d3.json(`/api/bubble_chart/${team}`).then(data => {
-//   console.log(data);
+  d3.json(`/api/bubble_chart/${team}`).then(data => {
+  console.log(data);
   
-//   let clusterLabel = [];
-//   clusterLabel =  data.map(row => row['cluster_label'])  
-//   console.log(clusterLabel);
+  let clusterLabel = [];
+  clusterLabel =  data.map(row => row['cluster_label'])  
+  console.log(clusterLabel);
+
+  console.log(data[0]['Points'])
+  console.log(data[5]['Points'])
 
 //   let points = [];
 //   data.forEach(row => {
@@ -193,137 +196,141 @@ function predTable(team) {
 // //   const h4container = document.getElementById("bubble-container");
 // //   h4container.appendChild(h4Element);
 // //   } else {
-//   var chartData = [
-//     {
-//       type: "indicator",
-//       mode: "number+gauge+delta",
-//       value: points[0],
-//       delta: { reference: sortedPoints[-1]},
-//       domain: { x: [0.25, 1], y: [0.05, 0.25] },
-//       title: { text: "Points" },
-//       gauge: {
-//         shape: "bullet",
-//         axis: { range: [null, sortedPoints[-1]+10] },
-//         // threshold: {
-//         //   line: { color: "black", width: 2 },
-//         //   thickness: 0.75,
-//         //   value: 170
-//         // },
-//         steps: [
-//           { range: [0, sortedPoints[0]], color: "white", line: {color: "black",  width: 2}},
-//           { range: [sortedPoints[0], sortedPoints[1]], color: "white", line: {color: "black",  width: 2}},
-//           { range: [sortedPoints[1], sortedPoints[2]], color: "white", line: {color: "black",  width: 2}},
-//           { range: [sortedPoints[2], sortedPoints[3]], color: "white", line: {color: "black",  width: 2}},
-//           { range: [sortedPoints[3], sortedPoints[4]], color: "white", line: {color: "black",  width: 2}},
-//           { range: [sortedPoints[4], sortedPoints[5]], color: "white", line: {color: "black",  width: 2}}           
-//         ],
-//         bar: { color: "black" }
-//       }
-//     },
-//     {
-//       type: "indicator",
-//       mode: "number+gauge+delta",
-//       value: wins[0],
-//       delta: { reference: sortedWins[-1]},
-//       domain: { x: [0.25, 1], y: [0.3, 0.5] },
-//       title: { text: "Wins" },
-//       gauge: {
-//         shape: "bullet",
-//         axis: { range: [null, sortedWins[-1]+5] },
-//         // threshold: {
-//         //   line: { color: "black", width: 2 },
-//         //   thickness: 0.75,
-//         //   value: 170
-//         // },
-//         steps: [
-//           { range: [0, sortedWins[0]], color: "white", line: {color: "black",  width: 2}},
-//           { range: [sortedWins[0], sortedWins[1]], color: "white", line: {color: "black",  width: 2}},
-//           { range: [sortedWins[1], sortedWins[2]], color: "white", line: {color: "black",  width: 2}},
-//           { range: [sortedWins[2], sortedWins[3]], color: "white", line: {color: "black",  width: 2}},
-//           { range: [sortedWins[3], sortedWins[4]], color: "white", line: {color: "black",  width: 2}},
-//           { range: [sortedWins[4], sortedWins[5]], color: "white", line: {color: "black",  width: 2}}           
-//         ],
-//         bar: { color: "black" }
-//       }
-//     },
-//     {
-//       type: "indicator",
-//       mode: "number+gauge+delta",
-//       value: goalsFor[0],
-//       delta: { reference: sortedGoalsFor[-1]},
-//       domain: { x: [0.25, 1], y: [0.55, 0.75] },
-//       title: { text: "Goals For" },
-//       gauge: {
-//         shape: "bullet",
-//         axis: { range: [null, sortedGoalsFor[-1]+10] },
-//         // threshold: {
-//         //   line: { color: "black", width: 2 },
-//         //   thickness: 0.75,
-//         //   value: 170
-//         // },
-//         steps: [
-//           { range: [0, sortedGoalsFor[0]], color: "white", line: {color: "black",  width: 2}},
-//           { range: [sortedGoalsFor[0], sortedGoalsFor[1]], color: "white", line: {color: "black",  width: 2}},
-//           { range: [sortedGoalsFor[1], sortedGoalsFor[2]], color: "white", line: {color: "black",  width: 2}},
-//           { range: [sortedGoalsFor[2], sortedGoalsFor[3]], color: "white", line: {color: "black",  width: 2}},
-//           { range: [sortedGoalsFor[3], sortedGoalsFor[4]], color: "white", line: {color: "black",  width: 2}},
-//           { range: [sortedGoalsFor[4], sortedGoalsFor[5]], color: "white", line: {color: "black",  width: 2}}           
-//         ],
-//         bar: { color: "black" }
-//       }
-//     },
-//     {
-//       type: "indicator",
-//       mode: "number+gauge+delta",
-//       value: goalsAgainst[0],
-//       delta: { reference: sortedGoalsAgainst[0]},
-//       domain: { x: [0.25, 1], y: [0.80, 1] },
-//       title: { text: "Goals Against" },
-//       gauge: {
-//         shape: "bullet",
-//         axis: { range: [null, sortedGoalsAgainst[-1]+10] },
-//         // threshold: {
-//         //   line: { color: "black", width: 2 },
-//         //   thickness: 0.75,
-//         //   value: 170
-//         // },
-//         steps: [
-//           { range: [0, sortedGoalsAgainst[0]], color: "white", line: {color: "black",  width: 2}},
-//           { range: [sortedGoalsAgainst[0], sortedGoalsAgainst[1]], color: "white", line: {color: "black",  width: 2}},
-//           { range: [sortedGoalsAgainst[1], sortedGoalsAgainst[2]], color: "white", line: {color: "black",  width: 2}},
-//           { range: [sortedGoalsAgainst[2], sortedGoalsAgainst[3]], color: "white", line: {color: "black",  width: 2}},
-//           { range: [sortedGoalsAgainst[3], sortedGoalsAgainst[4]], color: "white", line: {color: "black",  width: 2}},
-//           { range: [sortedGoalsAgainst[4], sortedGoalsAgainst[5]], color: "white", line: {color: "black",  width: 2}}           
-//         ],
-//         bar: { color: "black" }
-//       }
-//     }
-//   ];
+  var chartData = [
+    {
+      type: "indicator",
+      mode: "number+gauge+delta",
+      value: data[0]['Points'],
+      // value: points[0],
+      delta: { reference: data[5]['Points']},
+      domain: { x: [0.25, 1], y: [0.05, 0.20] },
+      title: { text: "Points" },
+      gauge: {
+        shape: "bullet",
+        axis: { range: [null, data[5]['Points']+10] },
+        // threshold: {
+        //   line: { color: "black", width: 2 },
+        //   thickness: 0.75,
+        //   value: 170
+        // },
+        // steps: [
+        //   { range: [0, sortedPoints[0]], color: "white", line: {color: "black",  width: 2}},
+        //   { range: [sortedPoints[0], sortedPoints[1]], color: "white", line: {color: "black",  width: 2}},
+        //   { range: [sortedPoints[1], sortedPoints[2]], color: "white", line: {color: "black",  width: 2}},
+        //   { range: [sortedPoints[2], sortedPoints[3]], color: "white", line: {color: "black",  width: 2}},
+        //   { range: [sortedPoints[3], sortedPoints[4]], color: "white", line: {color: "black",  width: 2}},
+        //   { range: [sortedPoints[4], sortedPoints[5]], color: "white", line: {color: "black",  width: 2}}           
+        // ],
+        bar: { color: "black" }
+      }
+    },
+    {
+      type: "indicator",
+      mode: "number+gauge+delta",
+      value: data[0]['Wins'],
+      // value: wins[0],
+      delta: { reference: data[5]['Wins']},
+      domain: { x: [0.25, 1], y: [0.35, 0.5] },
+      title: { text: "Wins" },
+      gauge: {
+        shape: "bullet",
+        axis: { range: [null, data[5]['Wins']+5] },
+        // threshold: {
+        //   line: { color: "black", width: 2 },
+        //   thickness: 0.75,
+        //   value: 170
+        // },
+        // steps: [
+        //   { range: [0, sortedWins[0]], color: "white", line: {color: "black",  width: 2}},
+        //   { range: [sortedWins[0], sortedWins[1]], color: "white", line: {color: "black",  width: 2}},
+        //   { range: [sortedWins[1], sortedWins[2]], color: "white", line: {color: "black",  width: 2}},
+        //   { range: [sortedWins[2], sortedWins[3]], color: "white", line: {color: "black",  width: 2}},
+        //   { range: [sortedWins[3], sortedWins[4]], color: "white", line: {color: "black",  width: 2}},
+        //   { range: [sortedWins[4], sortedWins[5]], color: "white", line: {color: "black",  width: 2}}           
+        // ],
+        bar: { color: "black" }
+      }
+    },
+    {
+      type: "indicator",
+      mode: "number+gauge+delta",
+      value: data[0]['Goals_For'],
+      // value: goalsFor[0],
+      delta: { reference: data[5]['Goals_For']},
+      domain: { x: [0.25, 1], y: [0.6, 0.75] },
+      title: { text: "Goals For" },
+      gauge: {
+        shape: "bullet",
+        axis: { range: [null, data[5]['Goals_For']+10] },
+        // threshold: {
+        //   line: { color: "black", width: 2 },
+        //   thickness: 0.75,
+        //   value: 170
+        // },
+        // steps: [
+        //   { range: [0, sortedGoalsFor[0]], color: "white", line: {color: "black",  width: 2}},
+        //   { range: [sortedGoalsFor[0], sortedGoalsFor[1]], color: "white", line: {color: "black",  width: 2}},
+        //   { range: [sortedGoalsFor[1], sortedGoalsFor[2]], color: "white", line: {color: "black",  width: 2}},
+        //   { range: [sortedGoalsFor[2], sortedGoalsFor[3]], color: "white", line: {color: "black",  width: 2}},
+        //   { range: [sortedGoalsFor[3], sortedGoalsFor[4]], color: "white", line: {color: "black",  width: 2}},
+        //   { range: [sortedGoalsFor[4], sortedGoalsFor[5]], color: "white", line: {color: "black",  width: 2}}           
+        // ],
+        bar: { color: "black" }
+      }
+    },
+    {
+      type: "indicator",
+      mode: "number+gauge+delta",
+      value: data[0]['Goals_Against'],
+      // value: goalsAgainst[0],
+      delta: { reference: data[5]['Goals_Against']},
+      domain: { x: [0.25, 1], y: [0.85, 1] },
+      title: { text: "Goals Against" },
+      gauge: {
+        shape: "bullet",
+        axis: { range: [null, data[5]['Goals_Against']+10] },
+        // threshold: {
+        //   line: { color: "black", width: 2 },
+        //   thickness: 0.75,
+        //   value: 170
+        // },
+        // steps: [
+        //   { range: [0, sortedGoalsAgainst[0]], color: "white", line: {color: "black",  width: 2}},
+        //   { range: [sortedGoalsAgainst[0], sortedGoalsAgainst[1]], color: "white", line: {color: "black",  width: 2}},
+        //   { range: [sortedGoalsAgainst[1], sortedGoalsAgainst[2]], color: "white", line: {color: "black",  width: 2}},
+        //   { range: [sortedGoalsAgainst[2], sortedGoalsAgainst[3]], color: "white", line: {color: "black",  width: 2}},
+        //   { range: [sortedGoalsAgainst[3], sortedGoalsAgainst[4]], color: "white", line: {color: "black",  width: 2}},
+        //   { range: [sortedGoalsAgainst[4], sortedGoalsAgainst[5]], color: "white", line: {color: "black",  width: 2}}           
+        // ],
+        bar: { color: "black" }
+      }
+    }
+  ];
 
-//   var layout = {
-//     width: 600, height: 250,
-//     margin: { t: 10, r: 25, l: 25, b: 10 },
-//     // xaxis: {
-//     //   tickvals: [
-//     //     [0, sortedPoints[0],sortedPoints[1],sortedPoints[2],sortedPoints[3],sortedPoints[4],sortedPoints[5] ],
-//     //     [0, sortedWins[0],sortedWins[1],sortedWins[2],sortedWins[3],sortedWins[4],sortedWins[5]],
-//     //     [0, sortedGoalsFor[0],sortedGoalsFor[1],sortedGoalsFor[2],sortedGoalsFor[3],sortedGoalsFor[4],sortedGoalsFor[5]],
-//     //     [0, sortedGoalsAgainst[0], sortedGoalsAgainst[1], sortedGoalsAgainst[2], sortedGoalsAgainst[3], sortedGoalsAgainst[4], sortedGoalsAgainst[5]]
-//     //   ],
-//     //   ticktext: [
-//     //     [0, sortedPoints[0],sortedPoints[1],sortedPoints[2],sortedPoints[3],sortedPoints[4],sortedPoints[5] ],
-//     //     [0, sortedWins[0],sortedWins[1],sortedWins[2],sortedWins[3],sortedWins[4],sortedWins[5]],
-//     //     [0, sortedGoalsFor[0],sortedGoalsFor[1],sortedGoalsFor[2],sortedGoalsFor[3],sortedGoalsFor[4],sortedGoalsFor[5]],
-//     //     [0, sortedGoalsAgainst[0], sortedGoalsAgainst[1], sortedGoalsAgainst[2], sortedGoalsAgainst[3], sortedGoalsAgainst[4], sortedGoalsAgainst[5]]
-//     //   ]
-//     // }
-//   };
+  var layout = {
+    width: 700, height: 250,
+    margin: { t: 10, r: 25, l: 25, b: 10 },
+    // xaxis: {
+    //   tickvals: [
+    //     [0, sortedPoints[0],sortedPoints[1],sortedPoints[2],sortedPoints[3],sortedPoints[4],sortedPoints[5] ],
+    //     [0, sortedWins[0],sortedWins[1],sortedWins[2],sortedWins[3],sortedWins[4],sortedWins[5]],
+    //     [0, sortedGoalsFor[0],sortedGoalsFor[1],sortedGoalsFor[2],sortedGoalsFor[3],sortedGoalsFor[4],sortedGoalsFor[5]],
+    //     [0, sortedGoalsAgainst[0], sortedGoalsAgainst[1], sortedGoalsAgainst[2], sortedGoalsAgainst[3], sortedGoalsAgainst[4], sortedGoalsAgainst[5]]
+    //   ],
+    //   ticktext: [
+    //     [0, sortedPoints[0],sortedPoints[1],sortedPoints[2],sortedPoints[3],sortedPoints[4],sortedPoints[5] ],
+    //     [0, sortedWins[0],sortedWins[1],sortedWins[2],sortedWins[3],sortedWins[4],sortedWins[5]],
+    //     [0, sortedGoalsFor[0],sortedGoalsFor[1],sortedGoalsFor[2],sortedGoalsFor[3],sortedGoalsFor[4],sortedGoalsFor[5]],
+    //     [0, sortedGoalsAgainst[0], sortedGoalsAgainst[1], sortedGoalsAgainst[2], sortedGoalsAgainst[3], sortedGoalsAgainst[4], sortedGoalsAgainst[5]]
+    //   ]
+    // }
+  };
 
-//   Plotly.newPlot('bubble-chart', chartData, layout);
+  Plotly.newPlot('bubble-chart', chartData, layout);
 // // }
 
-// })
-// };
+})
+};
 
 // Initialize the table with default league EPL
 
@@ -342,5 +349,5 @@ d3.selectAll('#selDataset').on('change', function () {
 d3.selectAll('#Teams').on('change', function () {
     console.log(this.value);
     predTable(this.value); 
-    // bulletChart(this.value);
+    bulletChart(this.value);
 });
